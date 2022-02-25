43e9b094e89370c434e88e74dca16811bc211a24
ahmed alsanhoty <ahmedalsanhoty47@gmail.com>
2022-02-25 14:52:13 +0100

    upload

---
+++ app.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import datetime
+import requests
+import pandas as pd
+'''
+# TaxiFareModel front
+'''
+
+st.markdown('''
+Remember that there are several ways to output content into your web page...
+
+Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+''')
+
+'''
+## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+
+1. Let's ask for:
+- date and time
+- pickup longitude
+- pickup latitude
+- dropoff longitude
+- dropoff latitude
+- passenger count
+'''
+pickup_date = st.date_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
+pickup_time = st.time_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
+pickup_datetime = f'{pickup_date} {pickup_time}'
+pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
+pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
+dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
+dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
+passenger_count = st.number_input('passenger_count', min_value=1, max_value=8, step=1, value=1)
+'''
+## Once we have these, let's call our API in order to retrieve a prediction
+
+
+See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+
+🤔 How could we call our API ? Off course... The `requests` package 💡
+'''
+
+url = 'https://taxifare.lewagon.ai/predict'
+
+if url == 'https://taxifare.lewagon.ai/predict':
+
+    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+
+'''
+
+2. Let's build a dictionary containing the parameters for our API...
+
+3. Let's call our API using the `requests` package...
+
+4. Let's retrieve the prediction from the **JSON** returned by the API...
+
+## Finally, we can display the prediction to the user
+'''
+params = dict(
+    pickup_datetime=pickup_datetime,
+    pickup_longitude=pickup_longitude,
+    pickup_latitude=pickup_latitude,
+    dropoff_longitude=dropoff_longitude,
+    dropoff_latitude=dropoff_latitude,
+    passenger_count=passenger_count)
+df = pd.DataFrame({'lon': [float(pickup_lon), float(dropoff_lon)],
+                   'lat': [float(pickup_lat), float(dropoff_lat)]})
+
+response = requests.get(url, params=params)
+
+prediction = response.json()
+
+pred = prediction['fare']
+
+pred
+st.write(pred)
+st.map(df)
